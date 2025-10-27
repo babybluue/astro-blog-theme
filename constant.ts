6316a0cd56fd15fef5151843b70ae7909cd4561c
babybluue <lijl@blackbeanssoft.com>
2025-10-27 11:11:16 +0800

    fix

--- constant.ts
+++ constant.ts
@@ -1,7 +1,7 @@
 import type { PostModel } from './src/interfaces/post-model'
 
 export const config = {
-  site: 'https://astro-blog-theme.vercel.app/',
+  site: 'https://astro-blog-theme.vercel.app',
   description:
     'Astro Blog Theme 是一个基于 Astro 的博客主题，支持 PWA、搜索、深色模式等功能，提供完整的 Markdown 语法支持。',
   siteName: 'Astro Blog Theme',
